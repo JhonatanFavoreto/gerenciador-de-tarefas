82bda46d7529e558d68cf7b3372096e020c812a9
Jhonatan <jhonatan.favoreto18@gmail.com>
2025-05-30 10:33:34 -0300

    Organize espaçamentos

--- Métodos-Array/tarefas.js
+++ Métodos-Array/tarefas.js
@@ -16,22 +16,26 @@ tarefas.push(`Jogar vídeo-game`);
 tarefas.push(`Assistir um filme`);
 console.log(`Depois do push:, ${tarefas}`);
 
+
 // Etapa 2: Removendo a Última Tarefa
 console.log(`Antes do pop:, ${tarefas}`);
 let tarefaRemovida = tarefas.pop();
 console.log(`Depois do pop:, ${tarefas}`);
 console.log(`Tarefa removida:, ${tarefaRemovida}`);
 
+
 // Etapa 3: Adicionando uma Tarefa no Início
 console.log(`Antes do unshift:, ${tarefas}`);
 tarefas.unshift(`Correr na rua`);
 console.log(`Depois do unshift:, ${tarefas}`);
+
 
 // Etapa 4: Removendo a Primeira Tarefa
 console.log(`Antes do shift:, ${tarefas}`);
 let tarefaRemovida2 = tarefas.shift();
 console.log(`Depois do shift:, ${tarefas}`);
 console.log(`Tarefa removida:, ${tarefaRemovida2}`);
+
 
 // Etapa 5: Verificando se uma Tarefa Existe
 console.log(
@@ -40,11 +44,14 @@ console.log(
   )}`
 );
 
+
 // Etapa 6: Transformando a Lista em String
 console.log(`Tarefas como string:, ${tarefas.join(',')}`);
 
+
 // Etapa 7: Extraindo uma Parte da Lista
 console.log(`Parte das Tarefas (índices 2 a 5): ${tarefas.slice(2, 5)}`);
+
 
 // Etapa 8: Alterando a Lista
 console.log(`Antes do splice: ${tarefas}`);
