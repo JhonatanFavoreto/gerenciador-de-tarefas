516e2c6941920cb639bf29fa5bf58d1376197b64
Jhonatan <jhonatan.favoreto18@gmail.com>
2025-05-30 09:24:34 -0300

    Altera lista

--- Métodos-Array/tarefas.js
+++ Métodos-Array/tarefas.js
@@ -1,21 +1,20 @@
 //  Etapa 1: Adicionando Novas Tarefas
 let tarefas = [
-    'Estudar JavaScript', 
-    'Fazer exercícios de matemática', 
-    'Lavar a louça', 
-    'Comprar mercado', 
-    'Ir à academia', 
-    'Ler um livro', 
-    'Fazer revisão para o teste', 
-    'Limpar o quarto', 
-    'Estudar para concurso', 
-    'Organizar o computador'
+  "Estudar JavaScript",
+  "Fazer exercícios de matemática",
+  "Lavar a louça",
+  "Comprar mercado",
+  "Ir à academia",
+  "Ler um livro",
+  "Fazer revisão para o teste",
+  "Limpar o quarto",
+  "Estudar para concurso",
+  "Organizar o computador",
 ];
 console.log("Antes do push:", tarefas);
-tarefas.push('Jogar vídeo-game');
-tarefas.push('Assistir um filme');
+tarefas.push("Jogar vídeo-game");
+tarefas.push("Assistir um filme");
 console.log("Depois do push:", tarefas);
-
 
 // Etapa 2: Removendo a Última Tarefa
 console.log("Antes do pop:", tarefas);
@@ -23,12 +22,10 @@ let tarefaRemovida = tarefas.pop();
 console.log("Depois do pop:", tarefas);
 console.log("Tarefa removida:", tarefaRemovida);
 
-
 // Etapa 3: Adicionando uma Tarefa no Início
 console.log("Antes do unshift:", tarefas);
-tarefas.unshift('Correr na rua');
+tarefas.unshift("Correr na rua");
 console.log("Depois do unshift:", tarefas);
-
 
 // Etapa 4: Removendo a Primeira Tarefa
 console.log("Antes do shift:", tarefas);
@@ -36,14 +33,23 @@ let tarefaRemovida2 = tarefas.shift();
 console.log("Depois do shift:", tarefas);
 console.log("Tarefa removida:", tarefaRemovida2);
 
-
 // Etapa 5: Verificando se uma Tarefa Existe
-console.log(`A lista de tarefas inclui 'Ir à academia'?, ${tarefas.includes("Ir à academia")}`);
-
+console.log(
+  `A lista de tarefas inclui 'Ir à academia'?, ${tarefas.includes(
+    "Ir à academia"
+  )}`
+);
 
 // Etapa 6: Transformando a Lista em String
-console.log("Tarefas como string:", tarefas.join(', '));
-
+console.log("Tarefas como string:", tarefas.join(", "));
 
 // Etapa 7: Extraindo uma Parte da Lista
 console.log(`Parte das Tarefas (índices 2 a 5): ${tarefas.slice(2, 5)}`);
+
+// Etapa 8: Alterando a Lista
+console.log(`Antes do splice: ${tarefas}`);
+let tarefaRemovida3 = tarefas.splice(3, 1);
+console.log(`Depois do splice: ${tarefas}`);
+tarefas[3] = "Limpar a casa"
+console.log(`Lista de tarefas após o acréscimo ${tarefas}`)
+console.log(`Tarefa adicionada ${tarefas[3]}`)
